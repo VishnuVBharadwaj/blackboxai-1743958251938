4da6782e11d205dd47d44e929790ed68d8e7e5eb
VishnuVBharadwaj <vishnubharadwajmadduri@gmail.com>
2025-04-06 22:21:11 +0530

    Add server.js 1743958270989

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const db = require('./db/db');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API Routes
+app.use('/api/tasks', require('./api/tasks'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  db.initializeDatabase();
+});
